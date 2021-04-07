f88e53d8c5aaec3cf6686f4c335c4f16c5ef7182
Raulf02 <70131993+Raulf02@users.noreply.github.com>
2021-04-07 18:18:00 +0200

    Session Handling

--- react-firebase-authentication/src/components/App/index.js
+++ react-firebase-authentication/src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
@@ -11,25 +11,48 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase'
+import { render } from '@testing-library/react';
 
-const App = () => (
-    <Router>
-        <div>
-            <Navigation />
+class App extends Component {
+    constructor(props) {
+    super(props);
 
-            <hr />
+    this.state = {
+        authUser: null,
+        };
+    }
 
-            <Route exact path={ROUTES.LANDING} component={LandingPage} />
-            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-            <Route path={ROUTES.PASSWORD_FORGOT} component={PasswordForgotPage} />
-            <Route path={ROUTES.HOME} component={HomePage} />
-            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-            <Route path={ROUTES.ADMIN} component={AdminPage} />
+    componentDidMount() {
+        this.props.firebase.auth.onAuthStateChanged(authUser => {
+            authUser
+            ? this.setState({ authUser })
+            : this.setState({ authUser: null });
+        });
+    }
 
-        </div>
-        
-    </Router>
-);
 
-export default App;
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Navigation authUser={this.state.authUser} />
+
+                    <hr />
+
+                    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+                    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+                    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+                    <Route path={ROUTES.PASSWORD_FORGOT} component={PasswordForgotPage} />
+                    <Route path={ROUTES.HOME} component={HomePage} />
+                    <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+                    <Route path={ROUTES.ADMIN} component={AdminPage} />
+
+                </div>
+                
+            </Router>
+        );
+    }
+}
+
+export default withFirebase(App);
--- react-firebase-authentication/src/components/Navigation/index.js
+++ react-firebase-authentication/src/components/Navigation/index.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut'
 import * as ROUTES from '../../constants/routes'
 
+const Navigation = ({ authUser }) => (
+    <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+)
 
-
-const Navigation = () => (
-    <div>
+const NavigationAuth = () => (
         <ul>
             <li>
                 <Link to={ROUTES.SIGN_IN}>Sign in</Link>
@@ -28,6 +29,16 @@ const Navigation = () => (
                 <SignOutButton />
             </li>
         </ul>
-    </div>
-);
+    );
+
+    const NavigationNonAuth = () => (
+        <ul>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+        </ul>
+    );
 export default Navigation;
